c7b37e33785178dee5f4276a3bdbe4cb938a19d8
Gavin Yuen <gaviny8@nycstudents.net>
2026-04-01 08:18:26 -0400

    sigh

---
+++ ReactFolder/my-app/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import Heading from './Heading.js';
+
+function App() {
+  return <><Heading name="Bqweob" /><Heading name="Jane" /></>
+}
+
+export default App;
