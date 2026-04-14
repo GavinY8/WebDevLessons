83a2954865c391e6103e01eb80c7a7550f4624c3
Gavin Yuen <gaviny8@nycstudents.net>
2026-04-14 09:31:18 -0400

    IM SO COOKED ACTUALLY

--- ReactFolder/my-app/src/App.js
+++ ReactFolder/my-app/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+import Card from './Card.js';
 import './App.css';
 import Heading from './Heading.js';
 
 function App() {
-  return <><Heading name="Bqweob" /><Heading name="Jane" /></>
+  return <h1>
+    <Card h2="First card's h2" h3="First card's h3"/>
+    <Card h2="Second card's h2" h3="Second card's h3"/>
+    <Card h2="Third card's h2" h3="Third card's h3"/>
+  </h1>
 }
 
 export default App;
